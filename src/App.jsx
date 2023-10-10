7cd1046363b1b0996a8522389ce50b669db71c70
Jadon Lau <jadon@Jadons-MacBook-Air.local>
2023-10-09 22:39:33 -0500

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,29 @@
 import React from 'react';
 import Banner from './components/Banner/Banner.jsx';
 import CourseList from './components/CourseList/CourseList.jsx';
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch';
+
+const FetchData = () => {
+  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+  if (error) return <span>error loading data</span>;
+  else if (isLoading) return <span>loading data</span>;
+  else if (!data) return <span>no data found</span>;
+  return (
+  <>
+    <Banner title={data.title} />
+    <CourseList courses={Object.values(data.courses)} />
+  </>
+  );
+}
+const queryClient = new QueryClient();
 
 const App = () => (
-  <div className="app-container">                      
-    <Banner title={schedule.title} />
-    <CourseList courses={schedule.courses} />
-  </div>
+  <QueryClientProvider client={queryClient}>
+    <div>
+      <FetchData />
+    </div>
+  </QueryClientProvider>
 );
 
 export default App;
