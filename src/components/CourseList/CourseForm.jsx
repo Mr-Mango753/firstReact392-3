440b34cc2397f4490d191f7dd16a5693de9fb54c
Jadon Lau <jadon@Jadons-MacBook-Air.local>
2023-10-16 18:18:23 -0500

    add form validation

--- src/components/CourseList/CourseForm.jsx
+++ src/components/CourseList/CourseForm.jsx
@@ -1,41 +1,39 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 import "./CourseForm.css";
 
-const InputField = ({ name, text, course }) => {
-    let defaultValue;
-  
-    if (name === "title") {
-      defaultValue = course.title;
-    } else if (name === "meets") {
-      defaultValue = course.meets;
-    } else {
-      defaultValue = "";
-    }
-  
-    return (
-      <div className="mb-3">
-        <label htmlFor={name} className="form-label">
-          {text}
-        </label>
-        <input
-          className="form-control"
-          id={name}
-          name={name}
-          defaultValue={defaultValue}
-        />
-      </div>
-    );
-  };
+const InputField = ({ name, text, course, onChange }) => {
+  let defaultValue;
+
+  if (name === "title") {
+    defaultValue = course.title;
+  } else if (name === "meets") {
+    defaultValue = course.meets;
+  } else {
+    defaultValue = "";
+  }
+
+  return (
+    <div className="mb-3">
+      <label htmlFor={name} className="form-label">
+        {text}
+      </label>
+      <input
+        className="form-control"
+        id={name}
+        name={name}
+        value={defaultValue} // Changed from 'defaultValue' to 'value' to make it controlled
+        onChange={onChange}  // Using the onChange prop
+      />
+    </div>
+  );
+};
 
   const ButtonBar = () => {
     const navigate = useNavigate();
   
     const handleCancelClick = () => {
-      navigate("/");
-    };
-  
-    const handleSubmitClick = () => {
       navigate("/");
     };
   
@@ -54,14 +52,61 @@ const InputField = ({ name, text, course }) => {
   };
   
 
-const CourseForm = ({ course }) => {
+const CourseForm = () => {
+  const [course, setCourse] = useState({ title: '', meets: '' });
+const [errors, setErrors] = useState({ title: '', meets: '' });
+
+function validateTitle(title) {
+  if (title.length < 2) {
+    return "Title must be at least two characters.";
+  }
+  return "";
+}
+
+function validateMeetingTime(meets) {
+  const regex = /^[MTWRF]+\s\d{2}:\d{2}-\d{2}:\d{2}$/;
+  if (meets === "") return "";
+  if (!regex.test(meets)) {
+    return "Must contain days and start-end, e.g., MWF 12:00-13:20";
+  }
+  return "";
+}
+
+function handleInputChange(event) {
+  const { name, value } = event.target;
+
+  // set the new input value
+  setCourse(prev => ({ ...prev, [name]: value }));
+
+  // validate the field
+  if (name === "title") {
+    setErrors(prev => ({ ...prev, title: validateTitle(value) }));
+  } else if (name === "meets") {
+    setErrors(prev => ({ ...prev, meets: validateMeetingTime(value) }));
+  }
+}
   return (
     <form noValidate>
-      <InputField name="title" text="Title" course={course} />
-      <InputField name="meets" text="Meeting Time" course={course} />
+      <InputField
+        name="title"
+        text="Title"
+        course={course}
+        onChange={handleInputChange}
+      />
+      {errors.title && <div className="error">{errors.title}</div>}
+  
+      <InputField
+        name="meets"
+        text="Meeting Time"
+        course={course}
+        onChange={handleInputChange}
+      />
+      {errors.meets && <div className="error">{errors.meets}</div>}
+  
       <ButtonBar />
     </form>
   );
+  
 };
 
 export default CourseForm;
