e655182f88e588d1a8e501b5577dcf14fdb415e3
Jadon <jadonlau2024@u.northwestern.edu>
2023-10-25 19:23:31 -0500

    add form submission

--- src/components/CourseList/CourseForm.jsx
+++ src/components/CourseList/CourseForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState } from 'react';
+import { useDbUpdate } from '../../utilities/firebase';
 import "./CourseForm.css";
 
 const InputField = ({ name, text, course, onChange }) => {
@@ -23,66 +24,86 @@ const InputField = ({ name, text, course, onChange }) => {
         className="form-control"
         id={name}
         name={name}
-        value={defaultValue} 
-        onChange={onChange} 
+        value={defaultValue}
+        onChange={onChange}
       />
     </div>
   );
 };
 
-  const ButtonBar = () => {
-    const navigate = useNavigate();
-  
-    const handleCancelClick = () => {
-      navigate("/");
-    };
-  
-    return (
-      <div className="d-flex">
-        <button
-          type="button"
-          className="btn btn-outline-dark me-2"
-          onClick={handleCancelClick}
-        >
-          Cancel
-        </button>
-  
-      </div>
-    );
+const ButtonBar = ({ onSubmit }) => {
+  const navigate = useNavigate();
+
+  const handleCancelClick = () => {
+    navigate("/");
   };
-  
+
+  return (
+    <div className="d-flex">
+      <button
+        type="button"
+        className="btn btn-outline-dark me-2"
+        onClick={handleCancelClick}
+      >
+        Cancel
+      </button>
+      <button
+        type="button"
+        className="btn btn-outline-dark me-2"
+        onClick={onSubmit}
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
 
 const CourseForm = () => {
+  const { courseId } = useParams();
+  const navigate = useNavigate();
   const [course, setCourse] = useState({ title: '', meets: '' });
-const [errors, setErrors] = useState({ title: '', meets: '' });
+  const [errors, setErrors] = useState({ title: '', meets: '' });
+  const [update, result] = useDbUpdate(`/courses/${courseId}`);
 
-function validateTitle(title) {
-  if (title.length < 2) {
-    return "Title must be at least two characters.";
+  const handleSubmitClick = async () => {
+    if (errors.title === "" && errors.meets === "") { 
+      try {
+        await update(course);
+        navigate("/");
+      } catch (error) {
+        console.error("Error updating course:", error);
+      }
+    }
+  };
+
+  function validateTitle(title) {
+    if (title.length < 2) {
+      return "Title must be at least two characters.";
+    }
+    return "";
   }
-  return "";
-}
 
-function validateMeetingTime(meets) {
-  const regex = /^[MTWRF]+\s\d{2}:\d{2}-\d{2}:\d{2}$/;
-  if (meets === "") return "";
-  if (!regex.test(meets)) {
-    return "Must contain days and start-end, e.g., MWF 12:00-13:20";
+  function validateMeetingTime(meets) {
+    const regex = /^[MTWRF]+\s\d{2}:\d{2}-\d{2}:\d{2}$/;
+    if (meets === "") return "";
+    if (!regex.test(meets)) {
+      return "Must contain days and start-end, e.g., MWF 12:00-13:20";
+    }
+    return "";
   }
-  return "";
-}
 
-function handleInputChange(event) {
-  const { name, value } = event.target;
+  function handleInputChange(event) {
+    const { name, value } = event.target;
 
-  setCourse(prev => ({ ...prev, [name]: value }));
+    setCourse(prev => ({ ...prev, [name]: value }));
 
-  if (name === "title") {
-    setErrors(prev => ({ ...prev, title: validateTitle(value) }));
-  } else if (name === "meets") {
-    setErrors(prev => ({ ...prev, meets: validateMeetingTime(value) }));
+    if (name === "title") {
+      setErrors(prev => ({ ...prev, title: validateTitle(value) }));
+    } else if (name === "meets") {
+      setErrors(prev => ({ ...prev, meets: validateMeetingTime(value) }));
+    }
   }
-}
+
   return (
     <form noValidate>
       <InputField
@@ -92,7 +113,6 @@ function handleInputChange(event) {
         onChange={handleInputChange}
       />
       {errors.title && <div className="error">{errors.title}</div>}
-  
       <InputField
         name="meets"
         text="Meeting Time"
@@ -100,11 +120,9 @@ function handleInputChange(event) {
         onChange={handleInputChange}
       />
       {errors.meets && <div className="error">{errors.meets}</div>}
-  
-      <ButtonBar />
+      <ButtonBar onSubmit={handleSubmitClick} />
     </form>
   );
-  
 };
 
 export default CourseForm;
